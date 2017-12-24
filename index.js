fd61fdc130d80321f190497d5f0af787f3216123
Joey Cho <cjy12182000@naver.com>
2017-12-24 00:25:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function fuzzyMatch(drivers,str){
   return drivers.filter(function(element){return element.startsWith(str)})
 }
 
-function matchName(){
-
+function matchName(drivers,str){
+  return drivers.filter(function(element){return element.name === str})
 }
