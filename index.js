0e4bbe90c0d9bc38f8b89bd293276e6259e618b1
Joey Cho <cjy12182000@naver.com>
2017-12-24 00:20:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Code your solution in this file
 function findMatching(drivers,str){
   list = drivers.filter(function(element){return element.toUpperCase()===str.toUpperCase()})
+  return list
 }
 
 function fuzzyMatch(){
-  
+
 }
 
 function matchName(){
-  
+
 }
